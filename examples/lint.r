e78f0f68fb163098b0d9f707a45b2c27f1f3ec94
Jonas Ackermann <jonas.p.ackermann@gmail.com>
2025-02-07 19:07:52 -0500

    try some more

--- examples/lint.r
+++ examples/lint.r
@@ -1,6 +1,13 @@
 # Until released, this needs to first be built locally, see buzzr directory and docs
+# Install devtools if you haven't already
+install.packages("devtools")
+
+# Install the package from the local directory
+devtools::install_local("../buzzr")
 
 library(buzzr)
 
+path <- "script.R"
+
 # Lint an R file and get positive comments
-lint_and_praise("path/to/your_script.R")
+lint_and_praise(path)
---
+++ examples/script.R
@@ -0,0 +1,8 @@
+# a good dumb function to lint and praise
+plus_one <- function(x) {
+  y <- x + 1
+  return(y)
+}
+
+test <- plus_one(1)
+print(test)
